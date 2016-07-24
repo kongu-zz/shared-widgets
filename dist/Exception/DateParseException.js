a0e4c538326a9648e7ab40c407c262dc64a135bb
Gennady Konovalov <kongu@mail.ru>
2016-07-24 14:15:02 +0300

    версия с хелперами

--- dist/Exception/DateParseException.js
+++ dist/Exception/DateParseException.js
@@ -1,4 +1,9 @@
 "use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var DateParseException = (function (_super) {
     __extends(DateParseException, _super);
     function DateParseException(message) {
